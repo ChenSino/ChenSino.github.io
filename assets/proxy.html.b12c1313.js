d5405efa824f45eb5cba40de8d54d0bee63123cb
zhuxiaofang1234 <819508408@qq.com>
2022-10-12 02:07:20 +0000

    Deploying to gh-pages from @ ChenSino/ChenSino.github.io@640aa911ecfc4ce440b81000fc8e06cb9114d2f2 🚀

---
+++ assets/proxy.html.b12c1313.js
@@ -0,0 +1,11 @@
+import{_ as a}from"./plugin-vue_export-helper.21dcd24c.js";import{o as s,c as t,b as e,a as i,e as o,d as c,r as l}from"./app.df44d27c.js";const r={},d=o(`<h2 id="_1\u3001\u5168\u5C40\u4EE3\u7406" tabindex="-1"><a class="header-anchor" href="#_1\u3001\u5168\u5C40\u4EE3\u7406" aria-hidden="true">#</a> 1\u3001\u5168\u5C40\u4EE3\u7406</h2><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code><span class="token function">git</span> config --global https.proxy http://127.0.0.1:1080
+<span class="token function">git</span> config --global https.proxy https://127.0.0.1:1080
+<span class="token comment">##\u53D6\u6D88\u4EE3\u7406</span>
+<span class="token function">git</span> config --global --unset http.proxy
+<span class="token function">git</span> config --global --unset https.proxy
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_2\u3001\u90E8\u5206\u4EE3\u7406" tabindex="-1"><a class="header-anchor" href="#_2\u3001\u90E8\u5206\u4EE3\u7406" aria-hidden="true">#</a> 2\u3001\u90E8\u5206\u4EE3\u7406</h2><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code><span class="token comment">#\u53EA\u5BF9github.com</span>
+<span class="token function">git</span> config --global http.https://github.com.proxy http://127.0.0.1:1080
+
+<span class="token comment">#\u53D6\u6D88\u4EE3\u7406</span>
+<span class="token function">git</span> config --global --unset http.https://github.com.proxy
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_3\u3001\u53C2\u8003" tabindex="-1"><a class="header-anchor" href="#_3\u3001\u53C2\u8003" aria-hidden="true">#</a> 3\u3001\u53C2\u8003</h2>`,5),p={href:"https://gist.github.com/laispace/666dd7b27e9116faece6",target:"_blank",rel:"noopener noreferrer"},h=c("git\u4EE3\u7406");function u(m,b){const n=l("ExternalLinkIcon");return s(),t("div",null,[d,e("p",null,[e("a",p,[h,i(n)])])])}var _=a(r,[["render",u],["__file","proxy.html.vue"]]);export{_ as default};
