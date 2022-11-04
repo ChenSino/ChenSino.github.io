d7312a60310f45b10bfa75dab6ab6e86edba2640
ChenSino <462488588@qq.com>
2022-11-04 03:46:59 +0000

    Deploying to gh-pages from @ ChenSino/ChenSino.github.io@ff299a5beb4de82994ac6f115efde5f0dc0c3820 🚀

--- assets/fatal.html.345a9f3e.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./plugin-vue_export-helper.21dcd24c.js";import{o as a,c as n,e as s}from"./app.6aa624c4.js";const i={},l=s(`<h2 id="\u5982\u4F55\u89E3\u51B3-fatal-unable-to-access-\u7684\u95EE\u9898" tabindex="-1"><a class="header-anchor" href="#\u5982\u4F55\u89E3\u51B3-fatal-unable-to-access-\u7684\u95EE\u9898" aria-hidden="true">#</a> \u5982\u4F55\u89E3\u51B3 fatal: unable to access...\u7684\u95EE\u9898</h2><p>\u4E00\uFF0C\u5728\u7EC8\u7AEF\u8F93\u5165</p><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code><span class="token function">ping</span> github.com
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><p>\u4E8C\uFF0C\u8BD5\u8BD5\u8FD9\u4E2A</p><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code>
-<span class="token function">git</span> config --global --unset http.proxy
-
-<span class="token function">git</span> config --global --unset https.proxy
-
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,5),c=[l];function t(d,o){return a(),n("div",null,c)}var p=e(i,[["render",t],["__file","fatal.html.vue"]]);export{p as default};
