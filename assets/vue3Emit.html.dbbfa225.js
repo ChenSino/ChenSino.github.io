1aea42aa5bd9e8611a5d44491572dfe1346c5141
ChenSino <462488588@qq.com>
2023-06-07 09:36:32 +0000

    Deploying to gh-pages from @ ChenSino/ChenSino.github.io@86b4f024ac06318f3df59d8e7e5db03792f22fbd 🚀

---
+++ assets/vue3Emit.html.dbbfa225.js
@@ -0,0 +1,32 @@
+import{_ as n}from"./plugin-vue_export-helper.21dcd24c.js";import{o as s,c as a,e as t}from"./app.d0f4ebc6.js";const e={},p=t(`<h2 id="vue3-\u4F7F\u7528\u7EC4\u5408\u5F0Fapi\u65F6\u5982\u4F55\u8FDB\u884C\u7236\u5B50\u7EC4\u4EF6\u901A\u4FE1" tabindex="-1"><a class="header-anchor" href="#vue3-\u4F7F\u7528\u7EC4\u5408\u5F0Fapi\u65F6\u5982\u4F55\u8FDB\u884C\u7236\u5B50\u7EC4\u4EF6\u901A\u4FE1" aria-hidden="true">#</a> vue3 \u4F7F\u7528\u7EC4\u5408\u5F0Fapi\u65F6\u5982\u4F55\u8FDB\u884C\u7236\u5B50\u7EC4\u4EF6\u901A\u4FE1</h2><div class="language-javascript ext-js line-numbers-mode"><pre class="language-javascript"><code><span class="token comment">// 1.\u5728vue\u7EC4\u4EF6\u4E2D\u5B9A\u4E49\u4E8B\u4EF6</span>
+<span class="token keyword">const</span> emit <span class="token operator">=</span> <span class="token function">defineEmits</span><span class="token punctuation">(</span><span class="token punctuation">[</span><span class="token string">&#39;update:hospitalDept&#39;</span><span class="token punctuation">]</span><span class="token punctuation">)</span>
+<span class="token comment">//2. \u8C03\u7528userXXXControl\u65F6\uFF0C\u628Aemit\u5F53\u505A\u53C2\u6570\u4F20\u9012\u8FDB\u53BB\uFF0CuseCreateHospitalDeptControl(emit)\u5B9E\u9645\u4E5F\u5C31\u7C7B\u4F3C\u4E00\u4E2A\u51FD\u6570</span>
+<span class="token keyword">const</span> <span class="token punctuation">{</span>
+  deptForm<span class="token punctuation">,</span>
+  formRules<span class="token punctuation">,</span>
+  validDeptRef<span class="token punctuation">,</span>
+  status<span class="token punctuation">,</span>
+  deptCategoryList<span class="token punctuation">,</span>
+  initColumn<span class="token punctuation">,</span>
+  initData<span class="token punctuation">,</span>
+  createHospitalDeptSubmit<span class="token punctuation">,</span>
+<span class="token punctuation">}</span> <span class="token operator">=</span> <span class="token function">useCreateHospitalDeptControl</span><span class="token punctuation">(</span>emit<span class="token punctuation">)</span>
+
+<span class="token comment">//3. \u5728useXXXContrl.tx\u6587\u4EF6\u4E2D\u8BA9\u51FD\u6570\u63A5\u6536\u4E00\u4E2A\u53C2\u6570</span>
+<span class="token keyword">const</span> <span class="token function-variable function">useCreateHospitalDeptControl</span> <span class="token operator">=</span> <span class="token punctuation">(</span><span class="token parameter"><span class="token literal-property property">emit</span><span class="token operator">:</span> any</span><span class="token punctuation">)</span><span class="token operator">=&gt;</span><span class="token punctuation">{</span>
+    
+    <span class="token comment">//\u8FD9\u91CC\u53EF\u4EE5\u4F7F\u7528vue\u7EC4\u4EF6\u4F20\u9012\u8FC7\u6765\u7684emit\u53C2\u6570\u4E86.....</span>
+   	<span class="token keyword">const</span> <span class="token function-variable function">testEmit</span> <span class="token operator">=</span> <span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token operator">=&gt;</span><span class="token punctuation">{</span>
+         <span class="token function">emit</span><span class="token punctuation">(</span><span class="token string">&#39;update:hospitalDept&#39;</span><span class="token punctuation">)</span>
+    <span class="token punctuation">}</span>
+    <span class="token keyword">return</span> <span class="token punctuation">{</span>
+        testEmit<span class="token punctuation">,</span>
+        <span class="token operator">...</span><span class="token punctuation">.</span>
+    <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+<span class="token comment">//4. \u7236\u7EC4\u4EF6xx.vue\u4E2D\u5C31\u53EF\u4EE5\u4F7F\u7528@update:hospital-depth</span>
+   <span class="token operator">&lt;</span>CreateHospitalDept
+      ref<span class="token operator">=</span><span class="token string">&quot;createHospitalDeptRef&quot;</span>
+      @update<span class="token operator">:</span>hospital<span class="token operator">-</span>dept<span class="token operator">=</span><span class="token string">&quot;updateHospitalDeptList&quot;</span>
+    <span class="token operator">/</span><span class="token operator">&gt;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),o=[p];function i(c,l){return s(),a("div",null,o)}var d=n(e,[["render",i],["__file","vue3Emit.html.vue"]]);export{d as default};
---
+++ assets/建造者模式.html.8241963d.js
@@ -0,0 +1,65 @@
+import{_ as n}from"./plugin-vue_export-helper.21dcd24c.js";import{o as s,c as a,e as t}from"./app.d0f4ebc6.js";const p={},e=t(`<h2 id="\u5EFA\u9020\u8005\u6A21\u5F0F" tabindex="-1"><a class="header-anchor" href="#\u5EFA\u9020\u8005\u6A21\u5F0F" aria-hidden="true">#</a> \u5EFA\u9020\u8005\u6A21\u5F0F</h2><p>\u8FD9\u79CD\u6A21\u5F0F\u592A\u5E38\u89C1\u4E86\uFF0C\u5F00\u6E90\u6846\u67B6\u4E2D\u7684\u5404\u79CDbuilder\u90FD\u662F</p><h2 id="mybatis\u4E2D" tabindex="-1"><a class="header-anchor" href="#mybatis\u4E2D" aria-hidden="true">#</a> mybatis\u4E2D</h2><p><code>org.apache.ibatis.mapping.Environment</code></p><div class="language-java ext-java line-numbers-mode"><pre class="language-java"><code><span class="token keyword">public</span> <span class="token keyword">final</span> <span class="token keyword">class</span> <span class="token class-name">Environment</span> <span class="token punctuation">{</span>
+  <span class="token keyword">private</span> <span class="token keyword">final</span> <span class="token class-name">String</span> id<span class="token punctuation">;</span>
+  <span class="token keyword">private</span> <span class="token keyword">final</span> <span class="token class-name">TransactionFactory</span> transactionFactory<span class="token punctuation">;</span>
+  <span class="token keyword">private</span> <span class="token keyword">final</span> <span class="token class-name">DataSource</span> dataSource<span class="token punctuation">;</span>
+
+  <span class="token keyword">public</span> <span class="token class-name">Environment</span><span class="token punctuation">(</span><span class="token class-name">String</span> id<span class="token punctuation">,</span> <span class="token class-name">TransactionFactory</span> transactionFactory<span class="token punctuation">,</span> <span class="token class-name">DataSource</span> dataSource<span class="token punctuation">)</span> <span class="token punctuation">{</span>
+    <span class="token keyword">if</span> <span class="token punctuation">(</span>id <span class="token operator">==</span> <span class="token keyword">null</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+      <span class="token keyword">throw</span> <span class="token keyword">new</span> <span class="token class-name">IllegalArgumentException</span><span class="token punctuation">(</span><span class="token string">&quot;Parameter &#39;id&#39; must not be null&quot;</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+    <span class="token keyword">if</span> <span class="token punctuation">(</span>transactionFactory <span class="token operator">==</span> <span class="token keyword">null</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+      <span class="token keyword">throw</span> <span class="token keyword">new</span> <span class="token class-name">IllegalArgumentException</span><span class="token punctuation">(</span><span class="token string">&quot;Parameter &#39;transactionFactory&#39; must not be null&quot;</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+    <span class="token keyword">this</span><span class="token punctuation">.</span>id <span class="token operator">=</span> id<span class="token punctuation">;</span>
+    <span class="token keyword">if</span> <span class="token punctuation">(</span>dataSource <span class="token operator">==</span> <span class="token keyword">null</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+      <span class="token keyword">throw</span> <span class="token keyword">new</span> <span class="token class-name">IllegalArgumentException</span><span class="token punctuation">(</span><span class="token string">&quot;Parameter &#39;dataSource&#39; must not be null&quot;</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+    <span class="token keyword">this</span><span class="token punctuation">.</span>transactionFactory <span class="token operator">=</span> transactionFactory<span class="token punctuation">;</span>
+    <span class="token keyword">this</span><span class="token punctuation">.</span>dataSource <span class="token operator">=</span> dataSource<span class="token punctuation">;</span>
+  <span class="token punctuation">}</span>
+
+  <span class="token keyword">public</span> <span class="token keyword">static</span> <span class="token keyword">class</span> <span class="token class-name">Builder</span> <span class="token punctuation">{</span>
+    <span class="token keyword">private</span> <span class="token keyword">final</span> <span class="token class-name">String</span> id<span class="token punctuation">;</span>
+    <span class="token keyword">private</span> <span class="token class-name">TransactionFactory</span> transactionFactory<span class="token punctuation">;</span>
+    <span class="token keyword">private</span> <span class="token class-name">DataSource</span> dataSource<span class="token punctuation">;</span>
+
+    <span class="token keyword">public</span> <span class="token class-name">Builder</span><span class="token punctuation">(</span><span class="token class-name">String</span> id<span class="token punctuation">)</span> <span class="token punctuation">{</span>
+      <span class="token keyword">this</span><span class="token punctuation">.</span>id <span class="token operator">=</span> id<span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+
+    <span class="token keyword">public</span> <span class="token class-name">Builder</span> <span class="token function">transactionFactory</span><span class="token punctuation">(</span><span class="token class-name">TransactionFactory</span> transactionFactory<span class="token punctuation">)</span> <span class="token punctuation">{</span>
+      <span class="token keyword">this</span><span class="token punctuation">.</span>transactionFactory <span class="token operator">=</span> transactionFactory<span class="token punctuation">;</span>
+      <span class="token keyword">return</span> <span class="token keyword">this</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+
+    <span class="token keyword">public</span> <span class="token class-name">Builder</span> <span class="token function">dataSource</span><span class="token punctuation">(</span><span class="token class-name">DataSource</span> dataSource<span class="token punctuation">)</span> <span class="token punctuation">{</span>
+      <span class="token keyword">this</span><span class="token punctuation">.</span>dataSource <span class="token operator">=</span> dataSource<span class="token punctuation">;</span>
+      <span class="token keyword">return</span> <span class="token keyword">this</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+
+    <span class="token keyword">public</span> <span class="token class-name">String</span> <span class="token function">id</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+      <span class="token keyword">return</span> <span class="token keyword">this</span><span class="token punctuation">.</span>id<span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+
+<span class="token comment">//  \u6700\u7EC8\u8FD4\u56DEEnvironment\u5BF9\u8C61</span>
+    <span class="token keyword">public</span> <span class="token class-name">Environment</span> <span class="token function">build</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+      <span class="token keyword">return</span> <span class="token keyword">new</span> <span class="token class-name">Environment</span><span class="token punctuation">(</span><span class="token keyword">this</span><span class="token punctuation">.</span>id<span class="token punctuation">,</span> <span class="token keyword">this</span><span class="token punctuation">.</span>transactionFactory<span class="token punctuation">,</span> <span class="token keyword">this</span><span class="token punctuation">.</span>dataSource<span class="token punctuation">)</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+
+  <span class="token punctuation">}</span>
+
+  <span class="token keyword">public</span> <span class="token class-name">String</span> <span class="token function">getId</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+    <span class="token keyword">return</span> <span class="token keyword">this</span><span class="token punctuation">.</span>id<span class="token punctuation">;</span>
+  <span class="token punctuation">}</span>
+
+  <span class="token keyword">public</span> <span class="token class-name">TransactionFactory</span> <span class="token function">getTransactionFactory</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+    <span class="token keyword">return</span> <span class="token keyword">this</span><span class="token punctuation">.</span>transactionFactory<span class="token punctuation">;</span>
+  <span class="token punctuation">}</span>
+
+  <span class="token keyword">public</span> <span class="token class-name">DataSource</span> <span class="token function">getDataSource</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+    <span class="token keyword">return</span> <span class="token keyword">this</span><span class="token punctuation">.</span>dataSource<span class="token punctuation">;</span>
+  <span class="token punctuation">}</span>
+
+<span class="token punctuation">}</span>
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,5),c=[e];function o(i,l){return s(),a("div",null,c)}var k=n(p,[["render",o],["__file","\u5EFA\u9020\u8005\u6A21\u5F0F.html.vue"]]);export{k as default};
